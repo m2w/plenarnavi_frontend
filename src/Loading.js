fa45e18a973b44e30c7fd8a0899e5d1f6151d3ec
Moritz <oss@tibidat.com>
2017-10-15 14:53:09 +0200

    Fix minor styling issues

--- src/Loading.js
+++ src/Loading.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import './Loading.css';
 
 class Loading extends Component {
--- src/Portrait.js
+++ src/Portrait.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import './Portrait.css';
 
 class Portrait extends Component {
